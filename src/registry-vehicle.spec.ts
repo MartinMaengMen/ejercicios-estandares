6277645db7a85a43601714811776e84fcda83820
Martín <u201520759@upc.edu.pe>
2022-11-28 02:52:18 -0500

    test: add test for registry-vehicle

---
+++ src/registry-vehicle.spec.ts
@@ -0,0 +1,46 @@
+import { RegistryVehicle } from "./registry-vehicle.js";
+
+describe('@RegistryVehicle',()=>{
+    it('Should return status code 400 when name does not exist',()=>{
+        const sut = new RegistryVehicle();
+        const httpRequest = {
+            body: {
+                name: 'Toyota',
+                model: 'Yaris',
+                year: 2022
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+        
+        expect(httpResponse.statusCode).toBe(400)
+    })
+ /*   const req = {name = 'pepito', lastname = 'mocha'}
+    describe('Initialization variables',()=>{
+        it('Deberia recibir como resultado el nombre de pepito'),()=>{
+            const req = {name: 'pepito', lastname : 'mocha'};
+    
+            const resultado = new RegistryVehicle(req).getName();
+            
+            expect(resultado).toBe('pepito')
+        }
+    })
+    describe('Callback APIs',()=>{
+        it('Deberia recibir como resultado el nombre de pepito'),()=>{
+            const req = {name: 'pepito', lastname : 'mocha'};
+    
+            const resultado = new RegistryVehicle(req).getName();
+            
+            expect(resultado).toBe('pepito')
+        }
+    })
+    describe('Callback Error APIs',()=>{
+        it('Deberia recibir como resultado el nombre de pepito'),()=>{
+            const req = {name: 'pepito', lastname : 'mocha'};
+    
+            const resultado = new RegistryVehicle(req).getName();
+            
+            expect(resultado).toBe('pepito')
+        }
+    })*/
+});
